9d42231db55ae3b64c0c45f07d44064fb2822d7b
dioh <dj.foguelman@gmail.com>
2017-10-06 14:53:25 -0300

    Fix a var_recursion method
    var_recursions method was throwing non-deterministig loglikelihood
    values as it was acumulating in a non-initialized value. Hence, causing
    random information to be added to the sum.

--- pyflux/var/var_recursions.pyx
+++ pyflux/var/var_recursions.pyx
@@ -42,7 +42,7 @@ def custom_covariance_matrix(np.ndarray[double,ndim=2] cov_matrix, int Y_len, in
 def var_likelihood(float ll1, int mu_shape, np.ndarray[double,ndim=2] diff, np.ndarray[double,ndim=2] inverse):
 
     cdef Py_ssize_t t
-    cdef float ll2
+    cdef float ll2 = 0
 
     for t in range(0,mu_shape):
         ll2 += np.dot(np.dot(diff[t].T,inverse),diff[t])
